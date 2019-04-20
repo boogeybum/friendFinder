fba1e20810aa89b360f0e8f3be34ac79a8c63b17
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-04-20 18:14:41 -0500

    set up application and listen with console log confirming success

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+// declare required dependencies
 var express = require("express");
 var path = require("path");
 
+// set up application with heroku compatible port
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+
+// log confirming PORT
+app.listen(PORT, function() {
+    console.log("App listening on port: " + PORT);
+});
