8de42288fed958d79465f23af8894816e2d10876
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-04-21 08:49:51 -0500

    Updated survey.html with non slim jquery version and added jquery for form validation and functionality. apiRoutes updated with routing logic. Currently only passing user input name, photo link and first question value.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,5 +6,49 @@ module.exports = function(app) {
     res.json(friends);
   });
 
-  
+  app.post("/api/friends", function(req, res) {
+    
+
+    // store user input (name, photo, scores)
+    //=============================================
+    var user = req.body;
+    console.log("After var declared: " + user.body);
+    // parseInt for scores
+    //=============================================
+    for(var i = 0; i < user.scores.length; i++) {
+      user.scores[i] = parseInt(user.scores[i]);
+      console.log("Inside for loop: " + user.body);
+    }
+
+    // default friend set, but will change to smallest score difference
+    //=============================================
+    var bestFriendIndex = 0;
+    var minimumDifference = 40;
+
+    // Zero difference to start then compare the user scores and the ith friend scores, one set at a time
+    //  differneces are added to total difference
+    //=============================================
+    for(var i = 0; i < friends.length; i++) {
+      var totalDifference = 0;
+      for(var j = 0; j < friends[i].scores.length; j++) {
+        var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+        totalDifference += difference;
+      }
+
+      // Update the best friend based on the current minimum difference
+      //=============================================
+      if(totalDifference < minimumDifference) {
+        bestFriendIndex = i;
+        minimumDifference = totalDifference;
+      }
+    }
+
+    // push matches to friend array
+    //=============================================
+    friends.push(user);
+
+    // return best friend result to browser
+    //=============================================
+    res.json(friends[bestFriendIndex]);
+  });
 };
