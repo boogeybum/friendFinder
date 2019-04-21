013fb37112b4d3081faf1e604c5a3daa9f2aeaa7
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-04-20 20:49:41 -0500

    added the first 5 friends to the list

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Peter Griffin",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/Peter_Griffin.png/220px-Peter_Griffin.png",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Barney Rubble",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Barney_Rubble.png/160px-Barney_Rubble.png",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Wrek it Ralph",
+		photo: "https://lumiere-a.akamaihd.net/v1/images/ralph-headretina_f6ef0c9b.jpeg?region=0,0,450,450&width=160",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Bob Parr aka Mr. Incredible",
+		photo: "https://vignette.wikia.nocookie.net/pixar/images/6/66/I2_-_Mr._Incredible_2.png/revision/latest?cb=20180620012113",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Tom Cat",
+		photo: "https://vignette.wikia.nocookie.net/tomandjerry/images/6/65/20140211071529%21Tom_Tom_and_Jerry.png/revision/latest?cb=20140529170742",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
